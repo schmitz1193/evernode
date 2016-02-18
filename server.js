a313d9c70f38ed81d40f1ec6606f7c578bdb205d
schmitz1193 <schmitz1193@gmail.com>
2016-02-18 10:53:37 -0600

    save to db

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const Note = mongoose.model('Notes', mongoose.Schema({
+  title: String,
+  text: String
+}));
 
 app.set('view engine', 'jade');  //all middleware must come before your routes
 
@@ -22,10 +26,16 @@ app.get('/notes/new', (req,res) => {  //serve up the form
 });
 
 app.post('/notes', (req,res) => {
-  console.log(req.body);
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+    console.log(note);
   res.redirect('/');
+  });
 });
-
-app.listen(port, () => {
+//wrap listen into the mongoose .connect
+mongoose.connect('mongodb://localhost:27017/evernode',(err) => {
+  if (err) throw err;
+  app.listen(port, () => {
   console.log(`Evernode server running on port: ${port}`);
-})
+  });
+});
