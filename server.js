8fb57f45b199ef98442db7e6ca0863b3ae55d6f5
schmitz1193 <schmitz1193@gmail.com>
2016-02-18 10:36:29 -0600

    commit second feature

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-// Evernode - server.js
-
 'use strict'
 
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const express = require('express');
-//const mongoose = require('mongoose');
-
-
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');  //all middleware must come before your routes
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));  //how do you want to handle the extended character set??
+
 app.get('/', (req, res) => {
   res.send('Server Running');
+});
+
+app.get('/notes/new', (req,res) => {  //serve up the form
+  res.render('new-note');
+});
+
+app.post('/notes', (req,res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
